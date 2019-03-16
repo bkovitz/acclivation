6b8b724567da16cf904d9168c63685952f041fcc
Dave Bender <dcbender@indiana.edu>
2019-03-16 18:13:19 -0400

    better (hopefully) colormap and range plot

--- plot_xyz.py
+++ plot_xyz.py
@@ -23,23 +23,32 @@ def read(filename, x_col, y_col, z_col):
   with open(filename, 'r') as csvfile:
     return parse(csvfile.read(), x_col, y_col, z_col)
 
+#see https://matplotlib.org/tutorials/colors/colormaps.html
+#
+#binary - little too white
+#ocean - nice green/blue
+#gist_stern - shows nice detail on the many bumps
+#rainbow - slightly better contrast within bumps than default
+#gnuplot2_r - white/yellow bumps stand out
+#gnuplot_r - everything clear
+#
 def plot(X, Y, Z, scatter, show=True, filename='', ylim1=-1, ylim2=1, xlim1=-1,
-        xlim2=1, zlim1=0, zlim2=11, cmapname='coolwarm', linewidth=0, elev=33.0,
-        azim=33.0, dpi=300, stride=1):
+        xlim2=1, zlim1=0, zlim2=11, cmapname='gnuplot_r', linewidth=0, elev=33.0,
+        azim=33.0, dpi=300, stride=1, pointsize=20):
+  cmap = plt.get_cmap(cmapname)
   fig = plt.figure()
   ax = Axes3D(fig)
   if scatter:
-    ax.scatter3D(X, Y, Z)
-    #ax.set_ylim((ylim1,ylim2))
-    #ax.set_xlim((xlim1,xlim2))
-    #ax.set_zlim((zlim1,zlim2))
+    #ax.scatter3D(X, Y, Z)
+    #ax.scatter3D(X, Y, Z, s=20, c=np.maximum(X+1.0, Y+1.0)/2.0, cmap='plasma', #perceptually uniform fade from origin to 1,1
+    ax.scatter3D(X, Y, Z, s=pointsize, c=np.maximum(X+1.0, Y+1.0)/2.0, cmap=cmap,
+            edgecolors='none')
   else:
     dim = int(sqrt(len(X)))
     print('dim : %dx%d' % (dim, dim))
     X = X.reshape((dim, dim))
     Y = Y.reshape((dim, dim))
     Z = Z.reshape((dim, dim))
-    cmap = getattr(cm, cmapname)
     ax.view_init(elev, azim)
     ax.plot_surface(X, Y, Z, rstride=stride, cstride=stride, cmap=cmap, linewidth=linewidth)
     #ax.plot_trisurf(X, Y, Z, cmap=cm.jet, edgecolor='none', shade=True)
